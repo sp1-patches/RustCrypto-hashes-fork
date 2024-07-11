8f6d303c0861ba7e5adcc36207c0f41fe5edaabc
mattstam <mattstam@live.com>
2024-07-10 17:25:07 -0700

    succinct patch sha3

--- sha3/src/state.rs
+++ sha3/src/state.rs
@@ -3,6 +3,13 @@ use core::convert::TryInto;
 const PLEN: usize = 25;
 const DEFAULT_ROUND_COUNT: usize = 24;
 
+#[cfg(all(
+    target_os = "zkvm",
+    target_vendor = "succinct",
+    target_arch = "riscv32"
+))]
+use crate::succinct;
+
 #[derive(Clone)]
 pub(crate) struct Sha3State {
     pub state: [u64; PLEN],
@@ -34,7 +41,7 @@ impl Sha3State {
             *s ^= u64::from_le_bytes(b.try_into().unwrap());
         }
 
-        keccak::p1600(&mut self.state, self.round_count);
+        self.permute();
     }
 
     #[inline(always)]
@@ -46,6 +53,22 @@ impl Sha3State {
 
     #[inline(always)]
     pub(crate) fn permute(&mut self) {
-        keccak::p1600(&mut self.state, self.round_count);
+        #[cfg(all(
+            target_os = "zkvm",
+            target_vendor = "succinct",
+            target_arch = "riscv32"
+        ))]
+        {
+            succinct::keccak_permute(&mut self.state);
+        }
+
+        #[cfg(not(all(
+            target_os = "zkvm",
+            target_vendor = "succinct",
+            target_arch = "riscv32"
+        )))]
+        {
+            keccak::p1600(&mut self.state, self.round_count);
+        }
     }
 }
