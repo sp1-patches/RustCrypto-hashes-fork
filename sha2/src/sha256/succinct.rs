8d1af11471820a1fe9677b93ac05482f957a8893
nhtyy <n@push0.dev>
2024-12-16 17:48:42 -0800

    chore(docs): syscall saftey

--- sha2/src/sha256/succinct.rs
+++ sha2/src/sha256/succinct.rs
@@ -1,5 +1,18 @@
 extern "C" {
+    /// Executes the SHA256 extend operation on the given word array.
+    ///
+    /// ### Safety
+    ///
+    /// The caller must ensure that `w` is valid pointer to data that is aligned along a four byte
+    /// boundary.
     fn syscall_sha256_extend(w: *mut u32);
+    
+    /// Executes the SHA256 compress operation on the given word array and a given state.
+    ///
+    /// ### Safety
+    ///
+    /// The caller must ensure that `w` and `state` are valid pointers to data that is aligned along a
+    /// four byte boundary.
     fn syscall_sha256_compress(w: *mut u32, state: *mut u32);
 }
 
--- sha3/src/succinct.rs
+++ sha3/src/succinct.rs
@@ -2,6 +2,12 @@ extern "C" {
     fn syscall_keccak_permute(state: *mut u64);
 }
 
+/// Executes the Keccak256 permutation on the given state.
+///
+/// ### Safety
+///
+/// The caller must ensure that `state` is valid pointer to data that is aligned along a four
+/// byte boundary.
 #[inline]
 pub(crate) fn keccak_permute(state: &mut [u64; 25]) {
     unsafe {
